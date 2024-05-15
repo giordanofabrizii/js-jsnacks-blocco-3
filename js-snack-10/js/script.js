00653a9637d9e72c59e1913f5b3f073215c412b6
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-15 12:19:10 +0200

    fixed script

--- js-snack-10/js/script.js
+++ js-snack-10/js/script.js
@@ -2,19 +2,19 @@
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
 let arrayUno = [3,5];
-let arrayDue = [235.5,6,8];
+let arrayDue = [235,5,6,8];
 
 let delta = arrayUno.length - arrayDue.length;
 
 if (delta > 0) { // caso in cui arrayUno e' piu grande di arrayDue
-    while (arrayDue.legnth === arrayUno.length) {
-        arrayDue.push(Math.floor(Math.random()*numbers.length + 1));
+    while (arrayDue.length !== arrayUno.length) {
+        arrayDue.push(Math.floor(Math.random()*100 + 1));
     }
 } else if (delta < 0) { // caso in cui arrayUno e' piu piccolo di arrayDue
-    while (arrayDue.legnth === arrayUno.length) {
-        arrayUno.push(Math.floor(Math.random()*numbers.length + 1));
+    while (arrayDue.length !== arrayUno.length) {
+        arrayUno.push(Math.floor(Math.random()*100 + 1));
     }
 }
 
 console.log(arrayDue);
-console.log(arrayUno)
+console.log(arrayUno);
