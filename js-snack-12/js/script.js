88f532b135be2ce139c7ffb8988cc8448bb828b3
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-16 11:36:24 +0200

    added bonus

--- js-snack-12/js/script.js
+++ js-snack-12/js/script.js
@@ -14,3 +14,29 @@ let testoA = "ciao";
 let testoB = "sono piu lungo";
 
 confronta(testoA, testoB);
+
+function controllaCaratteri(stringaUno, stringaDue) {
+    if (stringaUno.length === stringaDue.length) { // se sono uguali di lunghezza
+
+        let uguaglianza = true;
+
+        for (let i = 0; i < stringaDue.length; i++) {
+            if (stringaDue[i] != stringaUno[i]) {
+                uguaglianza = false;
+            }
+        }
+
+        if (uguaglianza) { // se sono uguali
+            console.log("sono uguali in tutto e per tutto");
+        } else { // se hanno qualche carattere diverso
+            console.log("sono lunghe uguali, ma sono diverse nei caratteri")
+        }
+    } else {
+        console.log("sono di diversa lunghezza");
+    }
+}
+
+let testoC = "ciao";
+let testoD = "ciao";
+
+controllaCaratteri(testoC, testoD);
