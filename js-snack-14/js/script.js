51a9c67b29dfca2fb563b259b8a686b98ac47928
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-21 12:33:23 +0200

    show in html

--- js-snack-14/js/script.js
+++ js-snack-14/js/script.js
@@ -1,15 +1,16 @@
 // ! Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
 
 let userNumber = Number.parseInt(prompt("Scrivi un numero"));
+const showEl = document.getElementById("show");
 
 if (isNaN(userNumber) || userNumber > 10) {
     userNumber = 5;
 }
 
 const timer = setInterval( function() {
-    console.log(userNumber)
     userNumber--;
+    showEl.innerHTML = userNumber;
     if (userNumber <= 0) {
         clearInterval(timer);
     }
-}, 1000, userNumber);
+}, 1000, userNumber, showEl);
